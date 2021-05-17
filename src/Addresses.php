3d6f83abc246601b9a82947dba1cadcced7fdf72
ben mckenzie <ben@ocular.co.nz>
2021-05-17 19:09:56 +1200

    remove jquery

--- src/Addresses.php
+++ src/Addresses.php
@@ -222,7 +222,6 @@ class Addresses_OrderForm extends Extension
 	public function updateFields($fields)
 	{
 
-		Requirements::javascript(THIRDPARTY_DIR . '/jquery/jquery.js');
 		Requirements::javascript(THIRDPARTY_DIR . '/jquery-entwine/dist/jquery.entwine-dist.js');
 		Requirements::javascript('swipestripe-addresses/javascript/Addresses_OrderForm.js');
 
